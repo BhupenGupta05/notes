fe0318a19b06eda4559c100432df50c012295b8c
bhupengupta05@gmail.com <bhupengupta05@gmail.com>
2023-11-14 15:48:02 +0530

    structure for backend application and testing

---
+++ backend/tests/reverse.test.js
@@ -0,0 +1,13 @@
+const reverse = require('../utils/for_testing.js').reverse;
+
+test('reverse of react', () => {
+    const result = reverse('react')
+
+    expect(result).toBe('tcaer')
+})
+
+test('reverse of releveler', () => {
+    const result = reverse('releveler')
+  
+    expect(result).toBe('releveler')
+})
