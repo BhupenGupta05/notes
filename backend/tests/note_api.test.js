2d3fa0fd5e7fd50d6725c28e41d919b27218b55c
bhupengupta05@gmail.com <bhupengupta05@gmail.com>
2023-11-23 14:33:34 +0530

    some changes

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -10,14 +10,11 @@ const Note = require('../models/note.js')
 
 beforeEach(async () => {
   await Note.deleteMany({})
-  console.log('cleared');
 
-  helper.initialNotes.forEach(async (note) => {
-    let noteObject = new Note(note)
-    await noteObject.save()
-    console.log('saved');
-  })
-  console.log('done');
+  const noteObjects = helper.initialNotes.map((note) => new Note(note))
+
+  const promiseArray = noteObjects.map((note) => note.save())
+  await Promise.all(promiseArray)
 })
 
 
