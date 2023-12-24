d8c3911a33d60b9688d201c862e36df88cbdb768
bhupengupta05@gmail.com <bhupengupta05@gmail.com>
2023-12-24 13:37:46 +0530

    Frontend Tests

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,8 @@ const Note = ({ note,toggleImportance }) => {
   const label = note.important ? 'make not important': 'make important'
 
   return (
-    <li className="my-2">{note.content}
+    <li className="my-2">
+      {note.content}
       <button onClick={toggleImportance} className="w-auto px-2 py-1 bg-slate-400 mx-2 rounded-sm">{label}</button>
     </li>
   )
